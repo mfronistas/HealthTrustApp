981fe8c1efb1fb61872a36d40df8179ff84ba629
Michael Fronistas <m.k.fronistas2@newcastle.ac.uk>
2021-12-17 18:05:51 +0000

    Routed error pages to app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
 from flask_login import LoginManager, current_user
 
-# Opening file to get database URL
-f = open('databaseURL.txt', 'r')
+
 
 # Function for custom decorator for roles
 def requires_roles(*roles):
@@ -18,7 +17,8 @@ def requires_roles(*roles):
         return wrapped
     return wrapper
 
-
+# Opening file to get database URL
+f = open('databaseURL.txt', 'r')
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f.readline()
@@ -26,10 +26,31 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'LongAndRandomSecretKey'
 db = SQLAlchemy(app)
 
-
+# Homepage
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# Error pages
+@app.errorhandler(403)
+def page_forbidden(error):
+    return render_template('403.html'), 403
+
+@app.errorhandler(400)
+def bad_request(error):
+    return render_template('400.html'), 400
+
+@app.errorhandler(503)
+def service_unavailable(error):
+    return render_template('503.html'), 500
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
