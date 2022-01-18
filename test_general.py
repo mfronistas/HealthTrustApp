1c0ae44b9561fda957145baca7e7e62eca818ddd
Zentaya <88230496+Zentaya@users.noreply.github.com>
2022-01-18 17:26:38 +0000

    new tests

--- test_general.py
+++ test_general.py
@@ -1,6 +1,5 @@
-from datetime import date
+from datetime import date, time
 from models import Prescription, User, Hospital, Appointment, Medicine
-
 
 
 def test_UserModel():
@@ -20,15 +19,35 @@ def test_UserModel():
     assert user.email == 'DJohn@email.com'
     assert user.password != 'Password'
 
-def test_HospitalModel():
 
-    pass
+def test_HospitalModel():
+    hospital = Hospital(name='Test Hospital', street='Test Street', postcode='NE7 1AY', city='Newcastle')
+    assert hospital.name == 'Test Hospital'
+    assert hospital.street == 'Test Street'
+    assert hospital.postcode == 'NE7 1AY'
+    assert hospital.city == 'Newcastle'
+
 
 def test_AppointmentModel():
-    pass
+    appointment = Appointment(patient_id=1, doctor_id='2', date=date(2022, 11, 24), time=time(14, 15),
+                              notes='pending', site_id=1)
+    assert appointment.patient_id == 1
+    assert appointment.doctor_id == 2
+    assert appointment.date == date(2022, 11, 24)
+    assert appointment.time == time(14, 15)
+    assert appointment.notes == 'pending'
+    assert appointment.site_id == 1
+
 
 def test_PrescriptionModel():
-    pass
+    prescription = Prescription(medicine_id=1, appointment_id=1, instructions='Twice a day')
+    assert prescription.medicine_id == 1
+    assert prescription.appointment_id == 1
+    assert prescription.instructions == 'Twice a day'
+
 
 def test_MedicineModel():
-    pass
+    medicine = Medicine(name='Moderna', type='Vaccine', dosage=5)
+    assert medicine.name == 'Moderna'
+    assert medicine.type == 'Vaccine'
+    assert medicine.dosage == 5
