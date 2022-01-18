e663170552dd41a06fc0f0b06f7e5cab228d2a19
jacob <c0051068@newcastle.ac.uk>
2022-01-18 17:04:28 +0000

    Added comment.

--- models.py
+++ models.py
@@ -176,6 +176,8 @@ def init_db():
     db.session.add(medicine)
     db.session.commit()
 
+
+# Function to create an appointment.
 def create_appointment():
     appointment = Appointment(patient_id=1, doctor_id=2, date=date(2022, 1, 23), time=time(9, 00), notes="", site_id=2)
     db.session.add(appointment)
